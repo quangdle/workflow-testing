605bcb4689946a18ad0b66fe4e761c73d7f72bfe
Quang Le <quang.ledinh@employmenthero.com>
2022-11-30 17:55:08 +0700

    3444

--- src/MailIntegrationModal.js
+++ src/MailIntegrationModal.js
@@ -148,7 +148,7 @@ const MailIntegrationModal = ({
               width: 200,
               margin: "auto",
               display: "block",
-              color: theme.colors.palette.greyLight45,
+              color: theme.colors.palette.greyLight40,
               backgroundColor: theme.colors.palette.redLight60,
             }}
           />
